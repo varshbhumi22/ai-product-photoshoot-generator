00da2058d9ed7bdd28a92b1f9cc22fb178012486
varshbhumi22 <varshneybhumi2005@gmail.com>
2026-03-12 14:09:22 +0530

    Created image_segmentation.py with ImageSegmentation class for background removal and product detection

---
+++ image_segmentation.py
@@ -0,0 +1,12 @@
+class ImageSegmentation:
+    def __init__(self):
+        # Initialization code (model loading, etc.)
+        pass
+
+    def remove_background(self, image):
+        # Code for background removal
+        pass
+
+    def detect_product(self, image):
+        # Code for product detection
+        pass
